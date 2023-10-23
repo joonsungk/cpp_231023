eaf2e908f82505f74a24f51dd3dbc7e046a4f3f3
Chansik Yun <chansigi@gmail.com>
2023-10-23 09:44:12 +0900

    .

--- 1_시작2.cpp
+++ 1_시작2.cpp
@@ -3,7 +3,35 @@
 #include <iostream> // C++ 표준 입출력 헤더
 // - C++의 표준 입출력 헤더 파일은 확장자를 사용하지 않습니다.
 
+#if 0
 int main()
 {
     std::cout << "Hello, C++" << std::endl;
+}
+#endif
+
+// 1. printf/scanf는 변수의 타입에 따라서 정확한 서식을 지정해야 합니다.
+//    std::cout/std::cin는 변수의 타입에 따라서 자동적으로 처리됩니다.
+
+// 2. std::cin을 사용할 때, 주소를 전달하지 않습니다.
+
+// 3. std::endl
+
+#if 0
+int main()
+{
+    long n = 42;
+
+    scanf("%ld", &n);
+    printf("%ld\n", n);
+}
+#endif
+
+int main()
+{
+    long n = 42;
+
+    std::cin >> n;
+
+    std::cout << n << std::endl;
 }
