a45423b443e13acb08dc132224f9566497dff275
Chansik Yun <chansigi@gmail.com>
2023-10-23 10:45:39 +0900

    .

--- 3_iostream.cpp
+++ 3_iostream.cpp
@@ -4,4 +4,19 @@ using namespace std;
 
 int main()
 {
+    // int n = 0xFAFA;
+    // 1111 1010 1111 1010
+    long long x = 10'000'000'000'000;
+
+    // C++11, 2진수 상수를 표현하는 기능이 추가되었습니다.
+    // 상수에 단일 따옴표를 이용해서 가독성을 높일 수 있습니다.
+    int n = 0b1111'1010'1111'1010;
+
+    printf("%x\n", n);
+    printf("%X\n", n);
+
+    // IO 조정자(Manipulator)
+    // : hex, uppercase
+    cout << hex << n << endl;
+    cout << hex << uppercase << n << endl;
 }
