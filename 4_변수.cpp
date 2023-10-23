73cdd560f3bcb6ff9571c68bf94fec178dcd8336
Chansik Yun <chansigi@gmail.com>
2023-10-23 11:08:52 +0900

    .

---
+++ 4_변수.cpp
@@ -0,0 +1,31 @@
+// 4_변수.cpp
+#include <iostream>
+using namespace std;
+
+struct Complex {
+    int re;
+    int im;
+};
+
+union Data {
+    int a;
+    double b;
+};
+
+enum State {
+    TASK_RUNNING,
+    TASK_STOPPED,
+};
+
+int main()
+{
+    // C 에서는
+    struct Complex c1;
+    union Data d1;
+    enum State s1;
+
+    // C++에서는 태그가 타입으로 인식됩니다.
+    Complex c2;
+    Data d2;
+    State s2;
+}
