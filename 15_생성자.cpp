bc6fd2863516aa70a51f3dcfde7787c733827bdd
Chansik Yun <chansigi@gmail.com>
2023-10-24 15:13:36 +0900

    .

--- 15_생성자.cpp
+++ 15_생성자.cpp
@@ -21,7 +21,7 @@ int main()
 
 // 2. 사용자가 생성자를 1개라도 제공하면,
 //    컴파일러는 기본 생성자를 제공하지 않습니다.
-
+#if 0
 class Point {
     int x;
     int y;
@@ -39,4 +39,34 @@ int main()
     Point pt { 10, 20 }; // -> Point(int, int)
 
     // Point pt2; // Error!
+}
+#endif
+
+// 3. 생성자는 인자 정보가 다를 경우, 여러 개 제공할 수 있습니다.
+//  ==> 생성자는 오버로딩이 가능합니다.
+//    "객체를 생성하는 다양한 방법을 제공할 수 있습니다."
+
+class Point {
+    int x;
+    int y;
+
+public:
+    Point(int a, int b)
+    {
+        x = a;
+        y = b;
+    }
+
+    Point()
+    {
+        x = 0;
+        y = 0;
+    }
+};
+
+int main()
+{
+    Point pt { 10, 20 }; // -> Point(int, int)
+
+    Point pt2; // --> Point()
 }
