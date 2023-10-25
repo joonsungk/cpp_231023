b0febeb38f3a13de2d73291cdca5fda7467ddc68
Chansik Yun <chansigi@gmail.com>
2023-10-25 16:40:47 +0900

    .

--- 25_상속3.cpp
+++ 25_상속3.cpp
@@ -12,6 +12,7 @@ using namespace std;
 //  2) protected: 외부 접근 불가능, 자식 클래스 접근 가능
 //  3) public: 외부 접근 가능
 
+#if 0
 class Animal {
 protected:
     int age = 42;
@@ -30,3 +31,32 @@ int main()
     Dog d;
     d.Print();
 }
+#endif
+
+// * 부모 클래스의 멤버 데이터도 반드시 private 만들어야 합니다.
+//   자식 클래스에서 부모 클래스의 멤버 데이터에 접근을, 멤버 함수를 통해서 수행하도록 만들어야 합니다.
+
+class Animal {
+private:
+    int agex = 42;
+
+protected:
+    int GetAge() const { return agex; }
+};
+
+class Dog : public Animal {
+public:
+    void Print() const
+    {
+        // cout << age << endl;
+        cout << GetAge() << endl;
+    }
+};
+
+int main()
+{
+    Dog d;
+    d.Print();
+}
+
+// Design Pattern 23가지 패턴, 에릭 감마
