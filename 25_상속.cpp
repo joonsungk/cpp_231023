0c7e6f90387d8bd7fd51cbb92e71bc03b12e00ed
Chansik Yun <chansigi@gmail.com>
2023-10-25 16:19:58 +0900

    .

---
+++ 25_상속.cpp
@@ -0,0 +1,70 @@
+// 25_상속.cpp
+#include <iostream>
+#include <string>
+using namespace std;
+
+// 상속(Inheritence)
+// => 연관된 클래스의 공통의 기능을 캡슐화할 수 있습니다.
+
+#if 0
+class Dog {
+    string name;
+    int age;
+
+public:
+    int GetAge() const { return age + 1; }
+    string GetName() const { return name; }
+};
+
+class Cat {
+    int age;
+    int speed;
+
+public:
+    int GetAge() const { return age + 1; }
+    int GetSpeed() const { return speed; }
+};
+
+int main()
+{
+    Dog d;
+    d.GetAge();
+    d.GetName();
+
+    Cat c;
+    c.GetAge();
+    c.GetSpeed();
+}
+#endif
+
+class Animal {
+    int age;
+
+public:
+    int GetAge() const { return age; }
+};
+
+class Dog : public Animal {
+    string name;
+
+public:
+    string GetName() const { return name; }
+};
+
+class Cat : public Animal {
+    int speed;
+
+public:
+    int GetSpeed() const { return speed; }
+};
+
+int main()
+{
+    Dog d;
+    d.GetAge();
+    d.GetName();
+
+    Cat c;
+    c.GetAge();
+    c.GetSpeed();
+}
