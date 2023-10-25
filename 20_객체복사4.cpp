7c8298ca2744d74fcf060ed93bae47d927f6cd73
Chansik Yun <chansigi@gmail.com>
2023-10-25 13:31:38 +0900

    .

---
+++ 20_객체복사4.cpp
@@ -0,0 +1,45 @@
+// 20_객체복사4.cpp
+#include <iostream>
+using namespace std;
+
+// => 복사 정책
+// 3) 복사 금지
+
+class User {
+    char* name;
+    int age;
+
+public:
+    // 컴파일러가 제공하는 복사 생성자 형태
+    User(const User& rhs)
+        : name { rhs.name }
+        , age { rhs.age }
+    {
+    }
+
+    User(const char* s, int n)
+        : age { n }
+    {
+        name = new char[strlen(s) + 1];
+        strcpy(name, s);
+    }
+
+    ~User()
+    {
+        delete[] name;
+    }
+
+    void Print()
+    {
+        cout << name << ", " << age << endl;
+    }
+};
+
+int main()
+{
+    User user { "Tom", 42 };
+    user.Print();
+
+    User other = user;
+    other.Print();
+}
