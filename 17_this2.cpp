f4bd05e3a75091c2a9c4f8b2e2a48cead6f3b703
Chansik Yun <chansigi@gmail.com>
2023-10-26 16:45:33 +0900

    .

--- 17_this2.cpp
+++ 17_this2.cpp
@@ -15,7 +15,7 @@ int main()
 } // ~Sample();
 #endif
 
-#if 1
+#if 0
 // private 소멸자
 //  의도 - 지역 객체와 전역 객체를 생성할 수 없고, 오직 new를 통해서만 객체를 생성할 수 있습니다.
 
@@ -50,3 +50,103 @@ int main()
     // Sample s; // Sample()
 } // ~Sample();
 #endif
+
+#include <string>
+
+#if 0
+// 아래 클래스의 수명을 참조 계수 기반으로 관리하고 싶습니다.
+class Image {
+    string url;
+
+    int ref;
+
+    ~Image()
+    {
+        cout << "Image 객체 파괴" << endl;
+    }
+
+public:
+    Image(const string& s)
+        : url { s }
+        , ref(1)
+    {
+    }
+
+    void AddRef() { ++ref; }
+    void Release()
+    {
+        if (--ref == 0) {
+            delete this;
+        }
+    }
+
+    void Draw() const { cout << "Draw Image: " << url << endl; }
+};
+
+int main()
+{
+    Image* p1 = new Image("https://a.com/a.jpg"); // 1
+
+    Image* p2 = p1; // 규칙: 포인터를 복사하면, 참조 계수 증가
+    p2->AddRef(); // 2
+
+    p1->Draw();
+    p2->Draw();
+
+    // 규칙: 포인터를 더이상 사용하지 않으면, 참조 계수 감소
+    p1->Release(); // 2 -> 1
+    p2->Release(); // 1 -> 0
+}
+#endif
+
+class RefBase {
+    int ref;
+
+public:
+    virtual ~RefBase() { }
+
+    RefBase()
+        : ref { 1 }
+    {
+    }
+
+    void AddRef() { ++ref; }
+    void Release()
+    {
+        if (--ref == 0) {
+            delete this;
+        }
+    }
+};
+
+class Image : public RefBase {
+    string url;
+
+    ~Image()
+    {
+        cout << "Image 객체 파괴" << endl;
+    }
+
+public:
+    Image(const string& s)
+        : url { s }
+    {
+    }
+
+    void Draw() const { cout << "Draw Image: " << url << endl; }
+};
+
+int main()
+{
+    Image* p1 = new Image("https://a.com/a.jpg"); // 1
+
+    Image* p2 = p1; // 규칙: 포인터를 복사하면, 참조 계수 증가
+    p2->AddRef(); // 2
+
+    p1->Draw();
+    p2->Draw();
+
+    // 규칙: 포인터를 더이상 사용하지 않으면, 참조 계수 감소
+    p1->Release(); // 2 -> 1
+    p2->Release(); // 1 -> 0
+}
