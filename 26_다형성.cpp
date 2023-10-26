eef018dcb7f628bc5a6a4b684f0c049e648282ac
Chansik Yun <chansigi@gmail.com>
2023-10-26 10:39:41 +0900

    .

--- 26_다형성.cpp
+++ 26_다형성.cpp
@@ -2,6 +2,24 @@
 #include <iostream>
 using namespace std;
 
+class Animal {
+};
+
+class Dog : public Animal { };
+
+// class Dog { };
+
 int main()
 {
+    Animal a;
+    Dog d;
+
+    Animal* p1 = &a;
+    Animal& r1 = a;
+
+    // - 부모 타입의 포인터/참조 타입으로 자식 객체를 참조할 수 있습니다.
+    // - 자식의 포인터/참조 타입은 부모 포인터/참조 타입으로의 암묵적인 변환을 허용합니다.
+    // => Upcasting
+    Animal* p2 = &d;
+    Animal& r2 = d;
 }
