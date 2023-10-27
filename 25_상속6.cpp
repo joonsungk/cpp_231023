f8a66c2a08022d8431eebd4ed0d80b4b36589652
Chansik Yun <chansigi@gmail.com>
2023-10-27 09:22:39 +0900

    .

--- 25_상속6.cpp
+++ 25_상속6.cpp
@@ -26,6 +26,7 @@ int main()
 }
 #endif
 
+#if 1
 // private 생성자
 // : 객체 생성의 정책을 제어하고 싶다.
 
@@ -61,3 +62,56 @@ int main()
     cout << &c1 << endl;
     cout << &c2 << endl;
 }
+#endif
+
+// 생성자 오버로딩 한계
+// 1) 동일한 인자 정보를 가지는 경우, 오버로딩이 불가능합니다.
+// 2) 생성자의 사용 형태는 항상 동일하기 때문에,
+//    객체 생성이 어떤 정책으로 수행되는지 확인하기 어렵습니다.
+// => 정적 팩토리 메소드
+//  Factory: 객체 생성 역활
+
+#if 0
+class User {
+    string id; // email, socialId
+public:
+    User(const std::string email) { }
+    // User(const std::string socialId) { }
+
+    User(int socialId) { }
+};
+
+int main()
+{
+    User user1 { "a@gmail.com" };
+    User user2 { 1213123123 };
+}
+#endif
+
+#if 0
+class User {
+    string id; // email, socialId
+
+    User(const string& i)
+        : id { i }
+    {
+    }
+
+public:
+    static User CreateUserWithEmail(const std::string& email)
+    {
+        return User(email);
+    }
+
+    static User CreateUserWithSocialId(int socialId)
+    {
+        return User(to_string(socialId));
+    }
+};
+
+int main()
+{
+    User user1 = User::CreateUserWithEmail("a@gmail.com");
+    User user2 = User::CreateUserWithSocialId(12312321);
+}
+#endif
